6113c6ef99e04c57bbe3ccdf321acf6e923592ce
acras01 <acras1@gmail.com>
2022-06-23 15:46:41 +0300

    Use actual priority value

--- app/src/main/java/ua/od/acros/zoningapp/misc/data/LocationRepositoryImpl.kt
+++ app/src/main/java/ua/od/acros/zoningapp/misc/data/LocationRepositoryImpl.kt
@@ -8,6 +8,7 @@ import android.location.Location
 import com.google.android.gms.location.FusedLocationProviderClient
 import com.google.android.gms.location.LocationRequest
 import com.google.android.gms.location.LocationServices
+import com.google.android.gms.location.Priority
 import com.google.android.gms.maps.model.LatLng
 import com.google.android.gms.tasks.CancellationTokenSource
 import com.google.android.gms.tasks.Continuation
@@ -35,7 +36,7 @@ class LocationRepositoryImpl @Inject constructor(
         var result: LatLng? = null
         suspendCoroutine<LatLng?> { cont ->
             fusedLocationClient?.getCurrentLocation(
-                LocationRequest.PRIORITY_HIGH_ACCURACY,
+                Priority.PRIORITY_HIGH_ACCURACY,
                 cancellationTokenSource.token
             )?.addOnCompleteListener { task: Task<Location> ->
                 if (task.isSuccessful) {
