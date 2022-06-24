6c99f9849fcf68aa864601c31d2c34766a316332
acras01 <acras1@gmail.com>
2022-06-24 21:59:47 +0300

    Add check if no zones with user parameters found

--- app/src/main/java/ua/od/acros/zoningapp/ui/main/ZonesMapFragment.kt
+++ app/src/main/java/ua/od/acros/zoningapp/ui/main/ZonesMapFragment.kt
@@ -4,6 +4,7 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
 import androidx.activity.OnBackPressedCallback
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.lifecycleScope
@@ -235,11 +236,14 @@ class ZonesMapFragment : Fragment() {
                     }
                 }
 
-                markerShown = 0
-                showZoneWithMarker(zonesList[markerShown])
-
-                if (zonesList.size > 1)
-                    binding.btnForward.isEnabled = true
+                if (zonesList.isNotEmpty()) {
+                    markerShown = 0
+                    showZoneWithMarker(zonesList[markerShown])
+                    if (zonesList.size > 1)
+                        binding.btnForward.isEnabled = true
+                } else {
+                    Toast.makeText(context, R.string.no_zones_found, Toast.LENGTH_LONG).show()
+                }
             }
         }
     }
